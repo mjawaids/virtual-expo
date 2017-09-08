0d763c889d4db0330b49227b265ea4c05a81a2b2
Muhammad Jawaid Shamshad <mjawaid@gmail.com>
2017-09-08 10:51:36 +0500

    Scheduled analytics email sending

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->command('email:send')->daily();
     }
 
     /**
